897d808c1478ed806e071ca2cd598c307085dae5
kimminsung <20210181@kumoh.ac.kr>
2025-07-17 15:14:07 +0900

    달리기 추가 완

--- Assets/Sprites/PlayerController.cs
+++ Assets/Sprites/PlayerController.cs
@@ -4,12 +4,19 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float runSpeed = 8f;     // 달리기 속도
     public float jumpForce = 12f;
     private Animator animator;
     private Rigidbody2D rb;
 
     private Vector2 moveInput;
     private bool isGrounded = true;
+    private bool isRunning = false;
+
+    private float lastInputTime = 0f;
+    private float doubleTapThreshold = 0.3f;  // 더블탭 허용 시간
+
+    private int lastDirection = 0; // -1: 왼쪽, 1: 오른쪽
 
     void Start()
     {
@@ -19,7 +26,25 @@ public class PlayerMovement : MonoBehaviour
 
     public void OnMove(InputValue value)
     {
-        moveInput = value.Get<Vector2>();
+        Vector2 input = value.Get<Vector2>();
+
+        if (input.x != 0)
+        {
+            int currentDirection = input.x > 0 ? 1 : -1;
+            if (currentDirection == lastDirection && Time.time - lastInputTime < doubleTapThreshold)
+            {
+                isRunning = true;
+            }
+            else
+            {
+                isRunning = false;
+            }
+
+            lastInputTime = Time.time;
+            lastDirection = currentDirection;
+        }
+
+        moveInput = input;
     }
 
     public void OnJump(InputValue value)
@@ -34,7 +59,8 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, rb.linearVelocity.y);
+        float currentSpeed = isRunning ? runSpeed : moveSpeed;
+        rb.linearVelocity = new Vector2(moveInput.x * currentSpeed, rb.linearVelocity.y);
         Flip();
         animator.SetBool("isMoving", moveInput.x != 0);
     }
@@ -53,7 +79,6 @@ public class PlayerMovement : MonoBehaviour
     {
         foreach (ContactPoint2D contact in collision.contacts)
         {
-            // contact.normal.y가 0.5 이상이면 바닥
             if (contact.normal.y > 0.5f && collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
             {
                 isGrounded = true;
