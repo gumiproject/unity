4c4735963ce4feae17bc420872caf90d42a03b6f
zetizom <90217209+zetizom@users.noreply.github.com>
2025-07-29 14:54:25 +0900

    토글 변수 교체

--- Assets/Scripts/SceneToggle.cs
+++ Assets/Scripts/SceneToggle.cs
@@ -4,19 +4,24 @@ using UnityEngine.InputSystem;
 
 public class SceneToggle : MonoBehaviour
 {
+    // 인스펙터에서 지정할 씬 이름 변수
+    public string sceneA;
+    public string sceneB;
+
     void Update()
     {
         if (Keyboard.current.nKey.wasPressedThisFrame)
         {
             string currentScene = SceneManager.GetActiveScene().name;
 
-            if (currentScene == "semple_post")
+            // 현재 씬이 sceneA와 같다면 sceneB를, 아니면 sceneA를 불러옴
+            if (currentScene == sceneA)
             {
-                SceneManager.LoadScene("semple_future");
+                SceneManager.LoadScene(sceneB);
             }
-            else if (currentScene == "semple_future")
+            else if (currentScene == sceneB)
             {
-                SceneManager.LoadScene("semple_post");
+                SceneManager.LoadScene(sceneA);
             }
         }
     }
