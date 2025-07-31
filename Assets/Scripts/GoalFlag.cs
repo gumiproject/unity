d278b87eb9e090ae4520337a48231270f1eb1259
zetizom <90217209+zetizom@users.noreply.github.com>
2025-07-31 16:38:23 +0900

    1-4 제작중
    1-4 제작중

--- Assets/Scripts/GoalFlag.cs
+++ Assets/Scripts/GoalFlag.cs
@@ -1,21 +1,7 @@
 using UnityEngine;
-using UnityEngine.SceneManagement; 
 
+// 이 스크립트는 이제 다음 씬의 이름 정보만 가지고 있습니다.
 public class GoalFlag : MonoBehaviour
 {
-    // 인스펙터 창에서 이동할 씬의 이름을 직접 입력받습니다.
-    public string kim_succes1;
-
-    // Is Trigger가 켜진 Collider에 다른 Collider가 들어왔을 때 자동으로 실행되는 함수입니다.
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        // 들어온 오브젝트의 태그가 "Player"인지 확인합니다.
-        if (other.CompareTag("Player"))
-        {
-            Debug.Log("플레이어가 도착했습니다!"); // 테스트용 로그
-
-            // 입력된 이름의 씬으로 이동합니다.
-            SceneManager.LoadScene(kim_succes1);
-        }
-    }
+    public string nextSceneName;
 }
